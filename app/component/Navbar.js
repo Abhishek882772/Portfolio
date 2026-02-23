2e0e2eae6a1a9098e2dc776dbfb21fb619ebfb41
Abhishek882772 <abhishek.23b1541075@abes.ac.in>
2026-02-23 15:57:09 +0530

    Add Link

--- app/component/Navbar.js
+++ app/component/Navbar.js
@@ -1,5 +1,6 @@
 "use client"
 import React, {useState} from 'react'
+import { Link } from "react-scroll";
 import content from './content'
 import Projects from './Projects'
 import Contact from './Contact'
@@ -11,15 +12,15 @@ const Navbar = () => {
             <div><img src="logo.png" alt="Logo" className=" h-40 filter invert" /></div>
             <div>
                 <nav className='flex gap-10 text-center items-center justify-center px-4'>
-                    <a className='transition-colors duration-200 ease-in-out hover:text-purple-800' href="http://localhost:3000">Home</a>
-                    <a className='transition-colors duration-200 ease-in-out hover:text-purple-800' href="http://localhost:3000/about">About</a>
-                    <a className='transition-colors duration-200 ease-in-out hover:text-purple-800' href="#projects">Projects</a>
-                    <a className='transition-colors duration-200 ease-in-out hover:text-purple-800' href="#skills">Skills</a>
+                    <Link className='transition-colors duration-200 ease-in-out hover:text-purple-800 hover:cursor-pointer' to="http://localhost:3000"  spy={true} smooth={true} duration={800}>Home</Link>
+                    <a className='transition-colors duration-200 ease-in-out hover:text-purple-800 hover:cursor-pointer' href="http://localhost:3000/about">About</a>
+                    <Link className='transition-colors duration-200 ease-in-out hover:text-purple-800 hover:cursor-pointer' to="projects"  spy={true} smooth={true} duration={800}>Projects</Link>
+                    <Link className='transition-colors duration-200 ease-in-out hover:text-purple-800 hover:cursor-pointer' to="skills"  spy={true} smooth={true} duration={800}>Skills</Link>
                     
                 </nav>
             </div>
             <div className='ml-10 flex justify-center bg-[#a7a7a7] text-black font-bold w-fit px-4 py-1 rounded-full
-             text-sm hover:bg-white  transition-colors duration-300 h-[40px] flex justify-center items-center'><a href="#contact">Contact Me</a></div>
+             text-sm hover:bg-white  transition-colors duration-300 h-[40px] flex justify-center items-center'><Link className='hover:cursor-pointer' to="contact"  spy={true} smooth={true} duration={800}>Contact Me</Link></div>
 
         </div>
     )
